d1caffbe5cf88dfae2c44061fa3597d9c90437c3
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-05-29 12:05:50 +0200

    Add prototype

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # split-password
+
+Prototype of a split-password scheme for password recovery in a semi-trusted environment.
+A password is split into shares using a trivial secret sharing scheme. You can only recover the password by combining all the shares.
+
+## Example
+```sh
+$ python split_password.py supersecret 3
+Password: supersecret
+Shares: ['WgxepHzwwN9xEA', '0T7UMP0hr-pPwg', '\x14F?UOk/|w\x06=']
+Recovered password: supersecret
+```
+
---
+++ split_password.py
@@ -0,0 +1,41 @@
+import argparse
+import binascii
+import secrets
+
+
+def xor_strings(s, t):
+    #return binascii.hexlify(''.join(chr(ord(a)^ord(b)) for a, b in zip(s, t)))
+    return ''.join(chr(ord(a)^ord(b)) for a, b in zip(s, t))
+
+parser = argparse.ArgumentParser(description="Split password into secret shares")
+parser.add_argument("password", type=str, help="Input password")
+parser.add_argument("n", type=int, help="Num of shares to split password into")
+
+args = parser.parse_args()
+
+password = args.password
+n_shares = args.n
+
+password_len = len(password)
+
+# Create shares
+shares = []
+last_share = password
+for i in range(n_shares-1):
+    secret = secrets.token_urlsafe(password_len-1)
+    shares.append(secret)
+    last_share = xor_strings(last_share, secret) 
+
+shares.append(last_share)
+
+
+# Recover password from shares
+recovered_password = shares[0]
+for i in range(1, len(shares)):
+    recovered_password = xor_strings(recovered_password, shares[i])
+
+
+print(f"Password: {password}")
+print(f"Shares: {shares}")
+print(f"Recovered password: {recovered_password}")
+
